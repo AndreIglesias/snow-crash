b21c8a27e2d3d66b936a6348a739eebfd05fc30e
ciglesia <35022933+AndreIglesias@users.noreply.github.com>
2023-02-18 16:47:37 +0100

    flag00

--- level00/resource/flag00.sh
+++ level00/resource/flag00.sh
@@ -6,18 +6,24 @@ echo "USER: $(whoami)"
 FILES=$(find / -user flag00 2>/dev/null)
 for file in $FILES
 do
-	printf "\n%-20s : %-10s\n" $file $(cat $file)
-	for rot in {0..26}
-	do
-		SOLVE=$(cat $file | tr "${DICT:0:26}" "${DICT:${rot}:26}")
-		echo -n $SOLVE $rot
-		if [ "$SOLVE" == "nottoohardhere" ]; then
-		   echo " ✔️"
-		else
-			echo
-		fi
-	done
+    if [ -f "$file" ];then
+        printf "\n%-20s : %-10s\n" $file $(cat $file)
+        for rot in {0..26}
+        do
+            SOLVE=$(cat $file | tr "${DICT:0:26}" "${DICT:${rot}:26}")
+            echo -n $SOLVE $rot
+            if [ "$SOLVE" == "nottoohardhere" ]; then
+                echo " ✔️"
+            else
+                echo
+            fi
+        done
+    fi
 done
 '
 
-sshpass -p level00 ssh level00@localhost -p 4242 "$SCRIPT"
+if [ ! -z "$1" ]; then
+	sshpass -p level00 ssh level00@$1 -p 4242 "$SCRIPT"
+else
+	echo "Usage: ./flag00.sh <ip address>"
+fi
