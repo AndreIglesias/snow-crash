3d218889198c924eb4b7ea26ff98422ca4da2033
ciglesia <35022933+AndreIglesias@users.noreply.github.com>
2023-02-23 21:17:46 +0100

    connect

--- connect.sh
+++ connect.sh
@@ -3,8 +3,16 @@
 LAST_EXERCISE=9
 
 if [ ! -z "$1" ] && [ ! -z "$2" ]; then
-	if [[ "$1" =~ ^[0-9]+$ ]] && ((0<= $1 && $1 <= $LAST_EXERCISE)); then
-		level$(printf '%02d' "$1")/resources/connect.sh $2
+	if [[ "$1" =~ ^[0-9]+$ ]] && ((0 <= $1 && $1 <= $LAST_EXERCISE)); then
+		NUM=$(printf '%02d' "$1")
+		LEVEL=level$NUM
+		PREVL=level$(printf "%02d" $((10#$NUM - 1)))
+		echo "Connecting to $LEVEL with $PREVL's flag..."
+		if (($1 == 0)); then
+			sshpass -p level00 ssh $LEVEL@$2
+		else
+			sshpass -p $(cat $PREVL/flag) ssh $LEVEL@$2
+		fi
 	else
 		echo "Enter a valid number between 0 and $LAST_EXERCISE"
 		echo "Usage ./connect.sh <Exercise number> <ip address>"
